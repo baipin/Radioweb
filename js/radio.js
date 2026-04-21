30c4eaa55c0d8bd5506bf0cff3eecaa26a0bb421
baipin <39830783+baipin@users.noreply.github.com>
2026-04-21 22:51:20 +0800

    Update radio.js

--- js/radio.js
+++ js/radio.js
@@ -129,4 +129,4 @@ const radioStations = [
     { name: "Big B Radio 日语流行", url: "http://jpop.bigbradio.net/s", logo: "", category: "网络音乐" }
 ];
 
-console.log("电台列表已加载：" + new Date().getTime());
+console.log("Loaded at" + new Date().getTime());
