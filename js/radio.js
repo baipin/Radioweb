753c20cc6c73439f9749dd3701a99fcf0e46605d
Dennis Wang <39830783+baipin@users.noreply.github.com>
2026-05-10 21:12:25 +0800

    Update radio.js

--- js/radio.js
+++ js/radio.js
@@ -50,7 +50,7 @@ var radioStations = [
     { name: "中国交通广播（湖南） China Traffic Radio (Hunan)", url: "http://living.muzhifm.com/muzhifm/ygfm_hn_fm905.m3u8?auth_key=1846587185-0-0-42f1728e48105df7491171fb8fcf67e5", logo: "https://radio.baipon.com/img/radio/CNTR.ico", category: "内地" },
     { name: "中国交通广播（四川） China Traffic Radio (Sichuan)", url: "http://living.muzhifm.com/muzhifm/bjnew_fm996.m3u8?auth_key=1786424188-0-0-6c56d523be4a13ebd6e8f425f6f6b8ae", logo: "https://radio.baipon.com/img/radio/CNTR.ico", category: "内地" },
     { name: "中国交通广播（江西） China Traffic Radio (Jiangxi)", url: "http://living.muzhifm.com/muzhifm/yangguang_fm1075.m3u8?auth_key=1846585491-0-0-dc02582dd9ae9eaef6ca2dc2c5371b67", logo: "https://radio.baipon.com/img/radio/CNTR.ico", category: "内地" },
-    { name: "中国交通广播（河北） China Traffic Radio (Jiangxi)", url: "http://living.muzhifm.com/muzhifm/ygfm_hb_fm1012.m3u8?auth_key=1846585491-0-0-5f9c1d2ea695ec9ca5893bf38d136332", logo: "https://radio.baipon.com/img/radio/CNTR.ico", category: "内地" },
+    { name: "中国交通广播（河北） China Traffic Radio (Hebei)", url: "http://living.muzhifm.com/muzhifm/ygfm_hb_fm1012.m3u8?auth_key=1846585491-0-0-5f9c1d2ea695ec9ca5893bf38d136332", logo: "https://radio.baipon.com/img/radio/CNTR.ico", category: "内地" },
 
 
     { name: "华语环球广播 Global Chinese Radio", url: "http://sk.cri.cn/hyhq.m3u8", logo: "https://www.cri.cn/favicon.ico", category: "内地" },
